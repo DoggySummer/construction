dd3fffa3079b2fb837a98f038bedc36a4b220736
doggysummer <matthew9304@gmail.com>
2025-04-24 08:51:14 +0900

    feat:lint정지

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ const nextConfig = {
 		domains: ['myexamplebucket1123.s3.ap-northeast-2.amazonaws.com'], // S3 버킷 도메인
 		formats: ['image/avif', 'image/webp'],
 	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 }
 export default nextConfig
